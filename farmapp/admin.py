ca6feee3a533d991a1fd5f2d9bad0bf7e6c19ddf
Mac M1 <macm1@Macs-MacBook-Pro.local>
2023-01-25 19:57:29 +0600

    prefinal edition

--- farmapp/admin.py
+++ farmapp/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import Incidences
+from .models import Farmer, Field, Culture, Season
 from leaflet.admin import LeafletGeoAdmin
 
 
-class IncidencesAmin(LeafletGeoAdmin):
+admin.site.register(Farmer)
+
+
+class FieldInfo(LeafletGeoAdmin):
     pass
+
+
+admin.site.register(Field, FieldInfo)
+admin.site.register(Season)
+admin.site.register(Culture)
+
+# class InfoAdmin(LeafletGeoAdmin):
+#     pass
     # list_display = ('name', 'location')
 
 
-admin.site.register(Incidences, IncidencesAmin)
+# admin.site.register(Incidences, InfoAdmin)
 
--- farmapp/models.py
+++ farmapp/models.py
@@ -3,13 +3,31 @@ from django.db import models
 from django.contrib.gis.db import models
 
 
-class Incidences(models.Model):
-    name = models.CharField(max_length=20)
-    location = models.PointField(srid=7694)
-    objects = models.Manager()
+class Farmer(models.Model):
+    name = models.CharField(max_length=50)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name_plural = "Incidences"
+
+class Culture(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Season(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Field(models.Model):
+    name = models.PolygonField(srid=4326)
+    objects = models.Manager()
+    farmer = models.ForeignKey(Farmer, null=True,  on_delete=models.CASCADE)
+    culture = models.ForeignKey(Culture, null=True, on_delete=models.CASCADE)
+    seasons = models.ForeignKey(Season, null=True, on_delete=models.CASCADE)
+
